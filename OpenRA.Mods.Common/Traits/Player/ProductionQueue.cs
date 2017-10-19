06b647b0f87fa733fe52762b26ac0d2a01bdf49c
abcdefg30 <alphabet@streber24.de>
2017-10-19 23:16:36 +0200

    Fix the "Unit Ready" notification playing erroneously

--- OpenRA.Mods.Common/Traits/Player/ProductionQueue.cs
+++ OpenRA.Mods.Common/Traits/Player/ProductionQueue.cs
@@ -373,7 +373,7 @@ namespace OpenRA.Mods.Common.Traits
 			if (!self.IsInWorld || self.IsDead)
 			{
 				CancelProduction(unit.Name, 1);
-				return true;
+				return false;
 			}
 
 			var sp = self.TraitsImplementing<Production>().FirstOrDefault(p => p.Info.Produces.Contains(Info.Type));
